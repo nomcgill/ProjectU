08806218d1e0cd1a7486ed60539f6de6887c9a6f
Nolan McGill <nomcgill@gmail.com>
2020-10-07 22:29:35 -0500

    html needed link fixes

--- site/src/reducer.js
+++ site/src/reducer.js
@@ -2,7 +2,7 @@ import * as actions from './actions';
 import sampleChoices from './sampleChoices'
 import sampleChoicesRevised from './sampleChoicesRevised.json'
 import cleanSlate from './cleanSlate.json'
-// import sampleProjectU from './sampleProjectU'
+import sampleProjectU from './sampleProjectU'
 
 // const initialState = sampleChoicesRevised
 const initialState = cleanSlate
